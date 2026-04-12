bc32c3a1cb6bf455989c36dbb302870ff1087a50
SenaAuliya <raffiakhdan12@gmail.com>
2026-04-12 12:49:05 +0700

    Initial commit baru

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sanity from "@sanity/astro";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+    sanity({
+      projectId: "0abpia8h",
+      dataset: "production",
+      useCdn: true,
+    }),
+  ],
+});
